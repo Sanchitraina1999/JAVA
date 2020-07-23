560aa49eac6575bbfd350564eaac3234672732b3
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-07-23 12:43:46 +0530

    --- Auto-Git Commit ---

--- input.java
+++ input.java
@@ -4,6 +4,7 @@ public class input{
     public static void main(String[] args){
         Scanner inp = new Scanner(System.in);
         System.out.println("Enter the val of a:");
-        int 
+        int a=Scanner.nextInt();
+        System.out.println("Enter the val of a:");
     }
 }
