6440dc1a00c51f23aebc805cd4e1eb8577c79d04
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-08-05 07:12:19 +0530

    --- Auto-Git Commit ---

--- array.java
+++ array.java
@@ -5,6 +5,7 @@ class array{
         for(int i=0;i<10;i++)
             ar[i]=i+1;
         for(int i=0;i<10;i++)
-            System.out.println(ar[i]+" ");
+            System.out.print(ar[i]+" ");
+        System.out.printlm()
     }
 }
