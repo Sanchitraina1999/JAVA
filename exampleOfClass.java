58231d3e3b6ed017468812a5b273c36b85179cd0
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-08-06 23:46:42 +0530

    --- Auto-Git Commit ---

--- exampleOfClass.java
+++ exampleOfClass.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 ;public class exampleOfClass {
     public static void main(String[] args){
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("Sanchit");
+        ArrayList<int> list = new ArrayList<int>();
+        list.add(1);
         while(list.hasMoreElements()){
-            Sy
+            System.out.print(list);
         }
     }
 }
