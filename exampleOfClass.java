c0df2828fe834b472f464cc9c6220aa5128a5ed9
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-08-06 23:39:36 +0530

    --- Auto-Git Commit ---

--- exampleOfClass.java
+++ exampleOfClass.java
@@ -2,7 +2,9 @@ public class exampleOfClass {
     public static void main(String[] args){
         class MathAdd{
             int num1,num;
-            
+            public int add(){
+                retur
+            }
         }
     }
 }
