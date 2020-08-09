57b92ea30f61485e21be956a0975242c706503b4
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-08-09 14:51:17 +0530

    --- Auto-Git Commit ---

---
+++ 1.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long t,count;
+    cin>>t;
+
+    while(t--)
+    {
+        string s,p,ans,res;
+        cin>>s>>p;
+
+        map<char,long long>mp;
+        vector<string>v1;
+
+
+         for(auto it : s)
+        {
+            mp[it]++;
+        }
+
+        for(auto it : p)
+        {
+            mp[it]--;
+        }
+        
+         for (auto itr = mp.begin(); itr != mp.end(); ++itr) 
+         { 
+        cout<< itr->first << " " << itr->second << '\n'; 
+        }
+
+        for(auto it : mp)
+        {
+            ans += it.first;
+        }
+        
+        count = ans.length();
+        res = ans;
+        
+        for(auto i=0; i<=count; i++)
+        {
+            res = ans;
+            res.insert(i,p);
+            v1.push_back(res);
+            res = '\0';
+        }
+        
+
+        for (auto i = v1.begin(); i != v1.end(); ++i) 
+        cout << *i << " "; 
+
+        //cout << min_element(v1.begin(), v1.end());/
+    }
+
+    return 0;
+}
