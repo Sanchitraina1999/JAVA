b552850d6fb394e34564b97f3cf2997fc0f8125b
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-08-09 14:40:00 +0530

    --- Auto-Git Commit ---

--- exampleOfClass.java
+++ exampleOfClass.java
@@ -8,20 +8,7 @@ public class exampleOfClass {
                 System.out.println("marks: "+ marks);
             }
         }
-        MathAdd addition = new MathAdd();//creates instance of the class i.e. object
-        addition.num1=10;
-        addition.num2=20;
-        System.out.println(addition.add());
-
-        class MathSub{
-            int num1,num2;
-            public int sub(){
-                return Math.abs(num1-num2);
-            }
-        }
-        MathSub Sub = new MathSub();//creates instance of the class i.e. object
-        Sub.num1=10;
-        Sub.num2=20;
-        System.out.println(Sub.sub());
+        student st=new student();
+        st
     }
 }
