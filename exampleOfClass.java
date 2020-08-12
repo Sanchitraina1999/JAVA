5317598bcd430b966b4bed6457fdccf4e85a2ce9
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-08-12 14:39:30 +0530

    --- Auto-Git Commit ---

--- exampleOfClass.java
+++ exampleOfClass.java
@@ -2,15 +2,19 @@ public class exampleOfClass {
     public static void main(String[] args){
         class student{
             String name;
-            float marks;
+            int marks;
             public void display(){
                 System.out.println("Name: " + name);
                 System.out.println("marks: "+ marks);
             }
         }
         student st1=new student();
-        st.name="Sanchit";
-        st.marks=10;
-        st.display();
+        st1.name="Sanchit";
+        st1.marks=10;
+        st1.display();
+        student st2=new student();
+        st2.name="Ayush";
+        st2.marks=20;
+        st2.display();
     }
 }
