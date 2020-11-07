c4efd317288c3368117ca25f01fabc0f5f68b68c
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-11-07 19:40:12 +0530

    :fire:

--- swing/addSwing.java
+++ swing/addSwing.java
@@ -8,30 +8,31 @@ public class addSwing {
     }
 }
 
-class abc extends JFrame implements ActionListener{
-    JLabel l1,l2,l3;
-    JTextField jt1,jt2;
+class abc extends JFrame implements ActionListener {
+    JLabel l1, l2, l3;
+    JTextField jt1, jt2;
     JButton b;
-    public abc(){
+
+    public abc() {
         setLayout(new FlowLayout());
 
         l1 = new JLabel("Number 1");
         l2 = new JLabel("Number 2");
         jt1 = new JTextField(20);
         jt2 = new JTextField(20);
-        l3 = new JLabel("Result ");
+        l3 = new JLabel("Result is: ");
         b = new JButton("Add");
-
 
         add(l1);
         add(jt1);
         add(l2);
         add(jt2);
 
-        add(b); 
-        add(l3);
+        add(b);
 
         b.addActionListener(this);
+
+        add(l3);
 
         setVisible(true);
         setSize(800, 600);
@@ -39,12 +40,12 @@ class abc extends JFrame implements ActionListener{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public void actionPerformed(ActionEvent e){
-        int a = Integer.parseInt(l1.getText());
-        int b = Integer.parseInt(l2.getText());
-        int res = a+b;
+    public void actionPerformed(ActionEvent e) {
+        int a = Integer.parseInt(jt1.getText());
+        int b = Integer.parseInt(jt2.getText());
+        int res = a + b;
 
-        l3.setText(""+res);
+        l3.setText(l3.getText() + res);
     }
 
 }
