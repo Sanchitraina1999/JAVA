2b870a33ab0b5dda1d8d665399d992a0b3178d74
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-11-07 23:36:28 +0530

    update: master

--- swing/calculator.java
+++ swing/calculator.java
@@ -10,8 +10,13 @@ public class calculator{
 class abc extends JFrame{
 
     JLabel l1,l2,choice;
+    JTextField t1,t2;
     abc(){
-        l1= new JLabel("")
+        l1= new JLabel("Number 1");
+        l2= new JLabel("Number 2");
+
+        add(l1);
+        add()
     }
 
     publi
