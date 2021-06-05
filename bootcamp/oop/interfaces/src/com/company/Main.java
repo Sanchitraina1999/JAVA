a3f2126b5a492fb6885b958f54bbdb5ea4835763
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-06-05 18:31:51 +0530

    :zap:

--- bootcamp/oop/interfaces/src/com/company/Main.java
+++ bootcamp/oop/interfaces/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        DeskPhone sanchitsPhone = new DeskPhone(123456);
+        sanchitsPhone.powerOn();
+        sanchitsPhone.callPhone(123456);
+        sanchitsPhone.answer();
     }
 }
